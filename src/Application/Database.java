0f2000f2fc90107d7b80abb6b55abcd96ba4eee8
James Hatfield <james.hatfield17@icloud.com>
2025-07-20 18:51:22 +0100

    Added Database class initializer used for fetching postgres credentials from properties file

--- src/Application/Database.java
+++ src/Application/Database.java
@@ -6,4 +6,24 @@ import java.sql.*;
 import java.util.Properties;
 
 public class Database {
+
+    private String dbURL;
+    private String dbUsername;
+    private String dbPassword;
+
+    // Used to fetch database credentials from properties file
+    public Database() {
+        Properties prop = new Properties();
+
+        try (InputStream input = new FileInputStream("db.properties")) {
+            prop.load(input);
+        } catch (Exception e) {
+            System.out.println("There was a problem fetching database credentials");
+            e.printStackTrace();
+        }
+
+        this.dbURL = prop.getProperty("db.url");
+        this.dbUsername = prop.getProperty("db.username");
+        this.dbPassword = prop.getProperty("db.password");
+    }
 }
