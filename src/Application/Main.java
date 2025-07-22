0d8b637b3074206cc7696405b4f598310bd81024
James Hatfield <james.hatfield17@icloud.com>
2025-07-22 11:59:15 +0100

    Added import of relevant javaFX packages

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,9 +1,17 @@
 package Application;
 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
 public class Main {
     public static void main(String[] args) {
-        UserInterface ui = new UserInterface();
+        // UserInterface ui = new UserInterface();
 
-        ui.start();
+        // ui.start();
     }
 }
