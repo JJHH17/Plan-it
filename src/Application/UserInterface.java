d686e8dc363936de87aa048554873fcef39ebeb2
James Hatfield <james.hatfield17@icloud.com>
2025-07-25 11:14:50 +0100

    Added JavaFX methods to UserInterface class

--- src/Application/UserInterface.java
+++ src/Application/UserInterface.java
@@ -1,75 +1,202 @@
 package Application;
 
-import java.util.Scanner;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import java.util.ArrayList;
 
-public class UserInterface {
-    private Scanner scanner;
-    private Database db;
+public abstract class UserInterface extends Application {
+    private Database db = new Database();
 
-    public UserInterface() {
-        this.scanner = new Scanner(System.in);
-        this.db = new Database();
-        db.createToDoTable();
+    @Override
+    public void start(Stage primaryStage) {
+        selectOption(primaryStage);
+
+        primaryStage.setTitle("Plan-It");
+        primaryStage.show();
     }
 
-    public void start() {
-        boolean active = true;
-        System.out.println("Welcome to Plan-It!");
+    public void selectOption(Stage primaryStage) {
+        Button addTask = new Button("Add Task");
+        Button viewTasks = new Button("View Tasks");
+        Button deleteTaskId = new Button("Delete Task (By Task ID)");
+        Button deleteTaskName = new Button("Delete Task (By Task name)");
+        Button deleteAllTasks = new Button("Delete All Tasks");
 
-        while (active) {
-            System.out.println("What would you like to do?");
-            System.out.println("1. Add a new task");
-            System.out.println("2. View all tasks");
-            System.out.println("3. Delete a Task (By Task ID)");
-            System.out.println("4. Delete a Task (By Task name)");
-            System.out.println("5. Delete all items");
-            System.out.println("6. Exit");
+        VBox initialStage = new VBox(10);
+        initialStage.getChildren().addAll(addTask, viewTasks, deleteTaskId, deleteTaskName, deleteAllTasks);
 
-            int choice = scanner.nextInt();
-            scanner.nextLine();
+        addTask.setOnAction(e -> {
+            addTaskUI(primaryStage);
+            initialStage.getChildren().clear();
+        });
 
-            switch (choice) {
-                case 1:
-                    System.out.println("Please enter the task name");
-                    String taskName = scanner.nextLine();
-                    System.out.println("Please enter the task description");
-                    String taskDescription = scanner.nextLine();
-                    System.out.println("Please enter the task priority (1-5)");
-                    int taskPriority = scanner.nextInt();
-                    ToDo task = new ToDo(taskName, taskDescription, taskPriority);
-                    db.addTask(task);
-                    break;
+        viewTasks.setOnAction(e -> {
+            displayAllTasks(primaryStage);
+            initialStage.getChildren().clear();
+        });
 
-                case 2:
-                    db.fetchTasks();
-                    break;
+        deleteTaskId.setOnAction(e -> {
+            idDeleteTaskUI(primaryStage);
+            initialStage.getChildren().clear();
+        });
 
-                case 3:
-                    System.out.println("Please enter the task ID to delete");
-                    int taskId = Integer.parseInt(scanner.nextLine());
-                    db.deleteTask(taskId);
-                    break;
+        deleteTaskName.setOnAction(e -> {
+            nameDeleteTaskUI(primaryStage);
+            initialStage.getChildren().clear();
+        });
 
-                case 4:
-                    System.out.println("Please enter the task name to delete");
-                    String taskNameToDelete = scanner.nextLine();
-                    db.deleteTask(taskNameToDelete);
-                    break;
+        deleteAllTasks.setOnAction(e -> {
+            deleteAllTasksUI(primaryStage);
+            initialStage.getChildren().clear();
+        });
 
-                case 5:
-                    System.out.println("Are you sure you want to delete all tasks? (Y/N)");
-                    char response = scanner.nextLine().toLowerCase().charAt(0);
-                    if (response == 'y') {
-                        db.deleteAllTasks();
-                    } else {
-                        System.out.println("Operation cancelled");
-                    }
-                    break;
+        Scene scene = new Scene(initialStage, 300, 250);
+        primaryStage.setScene(scene);
+    }
 
-                case 6:
-                    active = false;
-                    break;
-            }
+    public void addTaskUI(Stage primaryStage) {
+        // TODO: Allow user to go back to initial stage from this page
+
+        Label taskLabel = new Label("Task");
+        TextField taskField = new TextField();
+        Label descriptionLabel = new Label("Description");
+        TextField descriptionField = new TextField();
+        Label priorityLabel = new Label("Priority");
+        TextField priorityField = new TextField();
+
+        Button submitToDo = new Button("Submit");
+
+        VBox vbox = new VBox(10); // 10 = spacing between the elements
+        vbox.getChildren().addAll(taskLabel, taskField, descriptionLabel, descriptionField, priorityLabel, priorityField, submitToDo);
+
+        // Handles submission button
+        submitToDo.setOnAction(e -> {
+            String task = taskField.getText();
+            String description = descriptionField.getText();
+            int priority = priorityField.getText().isEmpty() ? 0 : Integer.parseInt(priorityField.getText());
+
+            // Feeds into DB function
+            ToDo newToDo = new ToDo(task, description, priority);
+            db.addTask(newToDo);
+
+            // Clears UI fields when submission is made
+            taskField.clear();
+            descriptionField.clear();
+            priorityField.clear();
+            // Clears existing items in scene
+            vbox.getChildren().clear();
+            selectOption(primaryStage);
+        });
+
+        Scene scene = new Scene(vbox, 300, 250);
+        primaryStage.setScene(scene);
+    }
+
+    public void idDeleteTaskUI(Stage primaryStage) {
+        Label idLabel = new Label("Delete a Task (By Task ID)");
+        Label deleteLabel = new Label("Please enter the task ID to delete");
+        TextField idField = new TextField();
+        Button deleteButton = new Button("Delete");
+        Button cancelButton = new Button("Back");
+
+        VBox vbox = new VBox(10);
+        vbox.getChildren().addAll(idLabel, deleteLabel, idField, deleteButton, cancelButton);
+
+        deleteButton.setOnAction(e -> {
+            int id = Integer.parseInt(idField.getText());
+            db.deleteTask(id);
+            vbox.getChildren().clear();
+            selectOption(primaryStage);
+        });
+
+        cancelButton.setOnAction(e -> {
+            vbox.getChildren().clear();
+            selectOption(primaryStage);
+        });
+
+        Scene scene = new Scene(vbox, 300, 250);
+        primaryStage.setScene(scene);
+    }
+
+    public void nameDeleteTaskUI(Stage primaryStage) {
+        Label nameLabel = new Label("Delete a Task (By Task name)");
+        Label deleteLabel = new Label("Please enter the task name to delete");
+        TextField nameField = new TextField();
+        Button deleteButton = new Button("Delete");
+        Button cancelButton = new Button("Back");
+
+        VBox vbox = new VBox(10);
+        vbox.getChildren().addAll(nameLabel, deleteLabel, nameField, deleteButton, cancelButton);
+
+        deleteButton.setOnAction(e -> {
+            String name = nameField.getText();
+            db.deleteTask(name);
+            vbox.getChildren().clear();
+            selectOption(primaryStage);
+        });
+
+        cancelButton.setOnAction(e -> {
+            vbox.getChildren().clear();
+            selectOption(primaryStage);
+        });
+
+        Scene scene = new Scene(vbox, 300, 250);
+        primaryStage.setScene(scene);
+    }
+
+    public void deleteAllTasksUI(Stage primaryStage) {
+        Label deleteAll = new Label("Are you sure you want to delete all tasks?");
+        Button confirmButton = new Button("Confirm");
+        Button cancelButton = new Button("Back");
+
+        VBox vbox = new VBox(10);
+        vbox.getChildren().addAll(deleteAll, confirmButton, cancelButton);
+
+        confirmButton.setOnAction(e -> {
+            db.deleteAllTasks();
+            vbox.getChildren().clear();
+            selectOption(primaryStage);
+        });
+
+        cancelButton.setOnAction(e -> {
+            vbox.getChildren().clear();
+            selectOption(primaryStage);
+        });
+
+        Scene scene = new Scene(vbox, 300, 250);
+        primaryStage.setScene(scene);
+    }
+
+    public void displayAllTasks(Stage primaryStage) {
+        // Using an arraylist to store DB entries
+        ArrayList<String> tasks = db.fetchTasks();
+
+        Label allTasks = new Label("Tasks:");
+        Label printTasks = new Label();
+        for (String task : tasks) {
+            printTasks.setText(printTasks.getText() + task + "\n");
         }
+
+        Button returnButton = new Button("Back");
+
+        VBox vbox = new VBox(10);
+        vbox.getChildren().addAll(allTasks, printTasks, returnButton);
+
+        returnButton.setOnAction(e -> {
+            vbox.getChildren().clear();
+            selectOption(primaryStage);
+        });
+
+        Scene scene = new Scene(vbox, 300, 250);
+        primaryStage.setScene(scene);
+    }
+
+    public void launchApp() {
+        launch();
     }
 }
