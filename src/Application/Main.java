74de0770d87a4f76d835ab2e9192cbb19d1112ec
James Hatfield <james.hatfield17@icloud.com>
2025-07-25 11:21:27 +0100

    Removed unneeded import methods from Main class now we've moved JavaFX calls to alternative class

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,14 +1,5 @@
 package Application;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
-import java.util.ArrayList;
-
 public class Main {
 
     public static void main(String[] args) {
